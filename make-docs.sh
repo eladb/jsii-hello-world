009e116ab07348f0b2b44c450fb28841bab8533a
Elad Ben-Israel <benisrae@amazon.com>
2018-09-12 15:32:17 +0300

    sphinx => docs

--- make-docs.sh
+++ make-docs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 version=$(node -e "console.log(require('./package.json').version)")
+mkdir -p dist/docs
 
-echo "<h1>Docs for version ${version}</h1>" > dist/sphinx/index.html
-echo "${version}" > dist/sphinx/.version
+echo "<h1>Docs for version ${version}</h1>" > dist/docs/index.html
+echo "${version}" > dist/docs/.version
 
